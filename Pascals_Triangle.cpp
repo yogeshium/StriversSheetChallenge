77bd3ddaf4bbfc759599d4d2e433024615c05ef6
yogeshium <112271449+yogeshium@users.noreply.github.com>
2023-06-09 17:22:09 +0530

    Create Pascals_Triangle.cpp

---
+++ Pascals_Triangle.cpp
@@ -0,0 +1,25 @@
+//Solution: 
+vector<vector<int>> generate(int n) 
+    {
+        vector<vector<int>> res;
+        res.push_back(vector<int>(1,1));
+
+        while(n>1)
+        {
+            vector<int> A = res.back();
+            vector<int> B;
+            for(int i=0;i<A.size();i++)
+            {
+                if(i-1<0)
+                    B.push_back(0+A[i]);
+                else
+                    B.push_back(A[i-1]+A[i]);
+            }
+            B.push_back(1);
+
+            res.push_back(B);
+            n--;
+        }
+
+        return res;    
+    }
